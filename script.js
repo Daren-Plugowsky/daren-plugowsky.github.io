18ff1ebb90f7a4e92265f882b1cc2323e3c461d8
Daren Plugowsky <159163712+Daren-Plugowsky@users.noreply.github.com>
2025-06-03 10:26:57 -0400

    added Unicode customization

--- script.js
+++ script.js
@@ -21,19 +21,20 @@ function printDuties(date) {
     var who = whoIsOnDuty(date);
     var result = "";
     if (who === "Daren") {
+        result += `${date}`
         result += "🟢 For doing the dishes, It's Daren's Turn<br>";
-        result += "For feeding the dog, It's Danika's Turn<br>";
-        result += "For undoing the dishes, It's Jaden's Turn<br>";
+        result += "⚪ For feeding the dog, It's Danika's Turn<br>";
+        result += "⚪ For undoing the dishes, It's Jaden's Turn<br>";
     
     }
     else if (who === "Danika") {
-        result += "For undoing the dishes, It's Daren's Turn<br>";
+        result += "⚪ For undoing the dishes, It's Daren's Turn<br>";
         result += "🟢 For doing the dishes, It's Danika's Turn<br>";
-        result += "For feeding the dog, It's Jaden's Turn<br>";
+        result += "⚪ For feeding the dog, It's Jaden's Turn<br>";
     }
     else if (who === "Jaden") {
-        result += "For feeding the dog, It's Daren's Turn<br>";
-        result += "For undoing the dishes, It's Danika's Turn<br>";
+        result += "⚪ For feeding the dog, It's Daren's Turn<br>";
+        result += "⚪ For undoing the dishes, It's Danika's Turn<br>";
         result += "🟢 For doing the dishes, It's Jaden's Turn<br>";
     }
     document.getElementById("result").innerHTML = result;
